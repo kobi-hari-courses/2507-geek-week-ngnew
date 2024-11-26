b72633a200ecf76c33bd480acdd325f22b0f8d46
Kobi Hari <kobi2294@yahoo.com>
2024-11-26 08:21:47 +0200

    ng new seminar - starting point

--- projects/ng-new/src/app/directives/my-link-class.token.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const MY_LINK_CLASS = new InjectionToken<string>('MY_LINK_CLASS');
--- projects/ng-new/src/app/vat-ratio.token.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const VAT_RATIO = new InjectionToken<number>('VAT_RATIO'); 
