2fbb59cd679eb560a63ababd9d8ef26895f6f47e
Kobi Hari <kobi2294@yahoo.com>
2025-07-14 12:03:06 +0300

    linked signal for selected id

--- projects/resources-books-store/src/app/services/state-service.ts
+++ projects/resources-books-store/src/app/services/state-service.ts
@@ -1,4 +1,10 @@
-import { Inject, Injectable, resource, signal } from '@angular/core';
+import {
+  Inject,
+  Injectable,
+  linkedSignal,
+  resource,
+  signal,
+} from '@angular/core';
 import { Book } from '../models/book';
 import { httpResource } from '@angular/common/http';
 
@@ -11,27 +17,56 @@ export class StateService {
 
   #keyword = signal<string>('the');
 
-  #searchResult = httpResource<Book[]>(() => ({
-    url: `${this.apiBase}/search`, 
-    params: { q: this.#keyword()}
-  }), {
-    defaultValue: []
-  })
+  #searchResult = httpResource<Book[]>(
+    () => ({
+      url: `${this.apiBase}/search`,
+      params: { q: this.#keyword() },
+    }),
+    {
+      defaultValue: [],
+    }
+  );
+
+  #selectedBookId = linkedSignal<Book[], string>({
+    source: () => this.searchResult.value(),
+    computation: (src, prev) => {
+      if (!prev) {
+        return src.length > 0 ? src[0].id : '';
+      }
+
+      if (prev.value === '' && src.length > 0) {
+        return src[0].id;
+      }
+
+      return prev.value;
+    },
+  });
 
   get keyword() {
     return this.#keyword.asReadonly();
   }
 
-    get searchResult() {
-        return this.#searchResult.asReadonly();
-    }
+  get searchResult() {
+    return this.#searchResult.asReadonly();
+  }
+
+  get selectedBookId() {
+    return this.#selectedBookId.asReadonly();
+  }
 
   setKeyword(value: string) {
     console.log('Keyword Changes to', value);
     this.#keyword.set(value);
   }
 
-  #searchKeywordPromise(value: string, abortSignal?: AbortSignal): Promise<Book[]> {
+  setSelectedBookId(value: string) {
+    this.#selectedBookId.set(value);
+  }
+
+  #searchKeywordPromise(
+    value: string,
+    abortSignal?: AbortSignal
+  ): Promise<Book[]> {
     return fetch(`${this.apiBase}/search?q=${value}`, {
       signal: abortSignal,
     }).then((resp) => resp.json());
