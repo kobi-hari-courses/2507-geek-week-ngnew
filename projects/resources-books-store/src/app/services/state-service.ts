b91580de6ca20d0b8ebc970377f3d79e5a565ccf
Kobi Hari <kobi2294@yahoo.com>
2025-07-14 11:49:05 +0300

    search result implemented with resource fetch

--- projects/resources-books-store/src/app/services/state-service.ts
+++ projects/resources-books-store/src/app/services/state-service.ts
@@ -1,22 +1,38 @@
-import { Inject, Injectable, signal } from "@angular/core";
+import { Inject, Injectable, resource, signal } from '@angular/core';
+import { Book } from '../models/book';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class StateService {
-    readonly apiBase = 'http://localhost:3000/api/books';
-    readonly wsBase = 'ws://localhost:3000/ws';
+  readonly apiBase = 'http://localhost:3000/api/books';
+  readonly wsBase = 'ws://localhost:3000/ws';
 
-    #keyword = signal<string>('the');
+  #keyword = signal<string>('the');
 
-    get keyword() {
-        return this.#keyword.asReadonly();
+  #searchResult = resource({
+    params: () => ({ keyword: this.#keyword() }),
+    loader: (options) => this.#searchKeywordPromise(options.params.keyword, options.abortSignal),
+    defaultValue: [],
+  });
+
+  get keyword() {
+    return this.#keyword.asReadonly();
+  }
+
+    get searchResult() {
+        return this.#searchResult.asReadonly();
     }
 
-    setKeyword(value: string) {
-        console.log('Keyword Changes to', value);
-        this.#keyword.set(value);
-    }
+  setKeyword(value: string) {
+    console.log('Keyword Changes to', value);
+    this.#keyword.set(value);
+  }
 
-    constructor(){}
+  #searchKeywordPromise(value: string, abortSignal?: AbortSignal): Promise<Book[]> {
+    return fetch(`${this.apiBase}/search?q=${value}`, {
+      signal: abortSignal,
+    }).then((resp) => resp.json());
+  }
+  constructor() {}
 }
